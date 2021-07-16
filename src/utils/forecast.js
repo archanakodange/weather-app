3d46f1586ed6a5f82369a0a3e238202f5ae527dc
Archana Kodange <kodange_archana@yahoo.com>
2021-07-16 12:58:34 +0530

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,35 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=909706802a660b78b18489e5276586e3&query=' + latitude + ',' + longitude;
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            const { weather_descriptions: weather, temperature, precip, humidity, wind_speed } = body.current;
+            const daysofWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+            
+            let dt = new Date();
+            let day = daysofWeek[dt.getDay()];
+            let hr = dt.getHours();
+            let min = dt.getMinutes();
+            if (min < 10) {
+                min = "0" + min;
+            }
+            let ampm = "am";
+            if( hr > 12 ) {
+                hr -= 12;
+                ampm = "pm";
+            }
+            callback(undefined, 'Weather on ' + day + ', ' + hr + ':' + min + ' ' + ampm + '\n' +
+                weather[0] + ' Temperature: ' + temperature + 
+                ' C, Percipitation: ' + precip + ' %, Humidity: ' + humidity +
+                ' %, Wind: ' + wind_speed + ' km/h');
+        }
+    });
+};
+
+module.exports = forecast;
---
+++ src/utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require('postman-request');
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYXJjaGFuYWtvZGFuZ2UiLCJhIjoiY2tyNGtiNmg5MmgyejJ1cGwxNzF3aG50cSJ9.tGqdogtf30wXbe7F2B9-4g&limit=1';
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined);
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined);
+        } else {
+            const { center, place_name } = body.features[0];
+            callback(undefined, {
+                latitude: center[1],
+                longitude: center[0],
+                location: place_name
+            });
+        }
+    });
+};
+
+module.exports = geocode;
